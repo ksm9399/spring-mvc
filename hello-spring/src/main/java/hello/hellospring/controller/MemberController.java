5742bb682f0f88f31866bc00f31e7aa2c7af73e3
ksm <rkdtjd0909@gmail.com>
2024-01-10 16:30:57 +0900

    section 5 - 회원 관리 예제_웹 MVC 개발
    - 회원 웹 기능_홈 화면 추가
    - 회원 웹 기능_등록
    - 회원 웹 기능_조회

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,7 +1,13 @@
 package hello.hellospring.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 
 /*
@@ -24,4 +30,26 @@ public class MemberController {
     this.memberService = memberService;
   }
 
+  @GetMapping("/members/new")
+  public String createForm() {
+    return "members/createMemberForm";
+  }
+
+  @PostMapping("/members/new")
+  public String create(MemberForm memberForm) {
+    Member member = new Member();
+    member.setName(memberForm.getName());
+
+    memberService.join(member);
+
+    return "redirect:/";
+  }
+
+  @GetMapping("/members")
+  public String list(Model model) {
+    List<Member> members = memberService.findMembers();
+    model.addAttribute("members", members);
+
+    return "members/memberList";
+  }
 }
