528e7bcdaa7e808fc85b05ae5431b029f3addc33
ksm <rkdtjd0909@gmail.com>
2024-01-11 16:35:27 +0900

    section 6 - 스프링 DB접근 기술
    - 스프링 데이터 JPA

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -17,26 +17,29 @@ import jakarta.persistence.EntityManager;
 public class SpringConfig {
 
   // private DataSource dataSource;
-  private EntityManager em;
+  // private EntityManager em;
+  private final MemberRepository memberRepository;
 
   public SpringConfig(
     // DataSource dataSource
-    EntityManager em
+    // EntityManager em
+    MemberRepository memberRepository
   ) {
     // this.dataSource = dataSource;
-    this.em = em;
+    // this.em = em;
+    this.memberRepository = memberRepository;
   }
 
   @Bean
   public MemberService memberService() {
-    return new MemberService(memberRepository());
+    return new MemberService(memberRepository);
   }
 
-  @Bean
-  public MemberRepository memberRepository() {
-    // return new MemoryMemberRepository();
-    // return new JdbcMemberRepository(dataSource);
-    // return new JdbcTemplateMemberRepository(dataSource);
-    return new JpaMemberRepository(em);
-  }
+  // @Bean
+  // public MemberRepository memberRepository() {
+  //   // return new MemoryMemberRepository();
+  //   // return new JdbcMemberRepository(dataSource);
+  //   // return new JdbcTemplateMemberRepository(dataSource);
+  //   return new JpaMemberRepository(em);
+  // }
 }
