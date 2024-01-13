7a8ef3dcbcb606d242ab10758c4273e0a0b00073
ksm <rkdtjd0909@gmail.com>
2024-01-14 01:36:23 +0900

    section 5 - 싱글톤 컨테이너
    -  싱글톤 패턴

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -27,4 +27,19 @@ public class SingletonTest {
     // memberService1 != memberService2
     Assertions.assertThat(memberService1).isNotSameAs(memberService2);
   }
+
+  @Test
+  @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+  void singletonServiceTest() {
+    SingletonService singletonService1 = SingletonService.getInstance();
+    SingletonService singletonService2 = SingletonService.getInstance();
+
+    System.out.println("singletonService1 = " + singletonService1);
+    System.out.println("singletonService2 = " + singletonService2);
+
+    // isSameAs - 인스턴스 비교
+    Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+
+    singletonService1.logic();
+  }
 }
