237ac1359ce1693e5694e4e7f1d40e8c96a3c9e4
ksm <rkdtjd0909@gmail.com>
2024-01-13 17:31:45 +0900

    section 3 - 스프링 핵심 원리 이해2_객체 지향 원리 적용
    - AppConfig 리팩터링

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -14,12 +16,20 @@ import hello.core.order.OrderServiceImpl;
  */
 public class AppConfig {
 
+  // MemoryMemberRepository() 이 부분이 중복 제거
+  private MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
+  }
+
+  private DiscountPolicy discountPolicy() {
+    return new FixDiscountPolicy();
+  }
 
   public MemberService memberService() {
-    return new MemberServiceImpl(new MemoryMemberRepository());
+    return new MemberServiceImpl(memberRepository());
   }
 
   public OrderService orderService() {
-    return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
 }
