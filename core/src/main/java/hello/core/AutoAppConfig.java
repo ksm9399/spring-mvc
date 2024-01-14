6ef7d5b2e7c99ff7bf8461186bf64098248e2352
ksm <rkdtjd0909@gmail.com>
2024-01-15 00:54:11 +0900

    section 6 - 컴포넌트 스캔
    - 컴포넌트 스캔과 의존관계 자동 주입 시작하기

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+// @Component 애노테이션이 붙은 클래스를 스캔해서 스프링 빈으로 등록
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+public class AutoAppConfig {
+
+
+}
--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,5 +1,8 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 /*
  * [회원 도메인 설계의 문제점]
  * OCP 원칙을 잘 준수하고 있는가?
@@ -12,6 +15,7 @@ package hello.core.member;
  * DIP(Dependency Inversion Principle - 의존 역전 원칙)란?
  * - 객체에서 어떤 Class를 참조해서 사용해야하는 상황이 생긴다면, 그 Class를 직접 참조하는 것이 아니라 그 대상의 상위 요소(추상 클래스 or 인터페이스)로 참조하라는 원칙
  */
+@Component
 public class MemberServiceImpl implements MemberService {
 
   // 의존관계가 인터페이스 뿐만 아니라 구현까지 모두 의존하는 문제점이 있음, (MemberRepository - 인터페이스, MemoryMemberRepository - 구현체)
@@ -20,6 +24,7 @@ public class MemberServiceImpl implements MemberService {
   // AppConfig 파일을 통해 외부에서 의존성을 주입해주기 때문에 DIP원칙 지킴 - 인터페이스에만 의존
   private final MemberRepository memberRepository;
 
+  @Autowired
   public MemberServiceImpl(MemberRepository memberRepository) {
     this.memberRepository = memberRepository;
   }
