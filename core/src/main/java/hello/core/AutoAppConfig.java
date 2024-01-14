8077718f315ef78d07f1a3d80f1ba5f48bc96d3e
ksm <rkdtjd0909@gmail.com>
2024-01-15 01:07:36 +0900

    section 6 - 컴포넌트 스캔
    - 탐색 위치와 기본 스캔 대상

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -5,7 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-// @Component 애노테이션이 붙은 클래스를 스캔해서 스프링 빈으로 등록
+/*
+ * @Component 애노테이션이 붙은 클래스를 스캔해서 스프링 빈으로 등록
+ * @SpringBootApplication 안에 @ComponentScan이 존재, 프로젝트 시작 루트에서 하위 루트까지 컴포넌트를 찾아서 스프링 컨테이너에 빈으로 등록됨
+ * @Component : 컴포넌트 스캔에서 사용
+ * @Configuration : 스프링 설정 정보로 인식하고, 스프링 빈이 싱글톤을 유지하도록 추가 처리
+ * @Controller : 스프링 MVC 컨트롤러로 인식
+ * @Service : 특별한 처리를 하지 않는다. 개발자들이 핵심 비즈니스 로직이 여기에 있겠구나 라고 비즈니스 계층을 인식하는데 도움
+ * @Repository : 스프링 데이터 접근 계층으로 인식하고, 데이터 계층의 예외를 스프링 예외로 변환
+ */
 @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
 
