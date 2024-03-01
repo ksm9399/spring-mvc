1e833bc888e4684375f12d49ad64dad2734b153a
ksm <rkdtjd0909@gmail.com>
2024-03-01 17:04:53 +0900

    상품목록, 상세 - 타임리프 적용

---
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,52 @@
+package hello.itemservice.web.basic;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor  // final이 붙은 멤버 필드 생성자를 만들어 줌
+public class BasicItemController {
+
+  private final ItemRepository itemRepository;
+
+  @GetMapping
+  public String items(
+    Model model
+  ) {
+    List<Item> items = itemRepository.findAll();
+
+    model.addAttribute("items", items);
+    return "basic/items";
+  }
+
+  @GetMapping("/{itemId}")
+  public String item(
+    @PathVariable Long itemId,
+    Model model
+  ) {
+    Item item = itemRepository.findById(itemId);
+
+    model.addAttribute("item", item);
+    return "basic/item";
+  }
+
+  /**
+   * 테스트용 데이터 추가
+   */
+  @PostConstruct
+  public void init() {
+    itemRepository.save(new Item("itemA", 10000, 10));
+    itemRepository.save(new Item("itemB", 20000, 20));
+  }
+}
