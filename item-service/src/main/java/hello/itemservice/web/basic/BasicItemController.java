1e3a7821349c0b0df34f636e25117e03be184767
ksm <rkdtjd0909@gmail.com>
2024-03-01 21:51:07 +0900

    PRG - POST, Redirect, GET

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -18,7 +18,7 @@ import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/basic/items")
-@RequiredArgsConstructor  // final이 붙은 멤버 필드 생성자를 만들어 줌
+@RequiredArgsConstructor // final이 붙은 멤버 필드 생성자를 만들어 줌
 public class BasicItemController {
 
   private final ItemRepository itemRepository;
@@ -50,24 +50,24 @@ public class BasicItemController {
   }
 
   // @PostMapping("/add")
-  // public String addItemV1(
-  //   @RequestParam String itemName,
-  //   @RequestParam int price,
-  //   @RequestParam Integer quantity,
-  //   Model model
-  // ) {
-  //   Item item = new Item();
+  public String addItemV1(
+    @RequestParam String itemName,
+    @RequestParam int price,
+    @RequestParam Integer quantity,
+    Model model
+  ) {
+    Item item = new Item();
 
-  //   item.setItemName(itemName);
-  //   item.setPrice(quantity);
-  //   item.setQuantity(quantity);
+    item.setItemName(itemName);
+    item.setPrice(quantity);
+    item.setQuantity(quantity);
 
-  //   itemRepository.save(item);
+    itemRepository.save(item);
 
-  //   model.addAttribute("item", item);
+    model.addAttribute("item", item);
 
-  //   return "basic/item";
-  // }
+    return "basic/item";
+  }
 
   /**
    * @ModelAttribute("item") Item item
@@ -75,15 +75,15 @@ public class BasicItemController {
    * @ModelAttribute("item") 선언된 네임명으로 model담아 뷰로 보내줌
    */
   // @PostMapping("/add")
-  // public String addItemV2(
-  //   @ModelAttribute("item") Item item,
-  //   Model model
-  // ) {
-  //   itemRepository.save(item);
-  //   // model.addAttribute("item", item);  // 자동 추가, 생략 가능
+  public String addItemV2(
+    @ModelAttribute("item") Item item,
+    Model model
+  ) {
+    itemRepository.save(item);
+    // model.addAttribute("item", item); // 자동 추가, 생략 가능
 
-  //   return "basic/item";
-  // }
+    return "basic/item";
+  }
 
   /**
    * @ModelAttribute name 생략 가능
@@ -91,23 +91,36 @@ public class BasicItemController {
    * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
    */
   // @PostMapping("/add")
-  // public String addItemV3(
-  //   @ModelAttribute Item item
-  // ) {
-  //   itemRepository.save(item);
+  public String addItemV3(
+    @ModelAttribute Item item
+  ) {
+    itemRepository.save(item);
 
-  //   return "basic/item";
-  // }
+    return "basic/item";
+  }
 
   /**
    * @ModelAttribute 자체 생략 가능
    * model.addAttribute(item) 자동 추가
    * 일반 자료형일 경우 @RequestParam 자체 생략됨 (int, String...)
    */
-  @PostMapping("/add")
+  // @PostMapping("/add")
   public String addItemV4(Item item) {
     itemRepository.save(item);
     return "basic/item";
+  }
+
+  /**
+   * PRG - Post/Redirect/Get
+   * 웹 브라우저의 새로 고침은 마지막에 서버에 전송한 데이터를 다시 전송
+   * addItemV4 리턴 값 뷰 호출 -> 웹 새로고침시 POST /add 상품 데이터를 서버로 전송됨
+   * id값만 다른 중복되는 데이터가 계속 쌓이는 문제 발생
+   * 저장 후 리다이렉트 통해 GET /items/{id} 호출 -> 새로고침 해도 GET /items/{id} 호출 됨
+   */
+  // @PostMapping("/add")
+  public String addItemV5(Item item) {
+    itemRepository.save(item);
+    return "redirect:/basic/items/" + item.getId();
   }
 
   @GetMapping("/{itemId}/edit")
