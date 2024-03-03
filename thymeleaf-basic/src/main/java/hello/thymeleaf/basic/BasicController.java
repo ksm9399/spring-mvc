52ddee130bcd4f31be6500f80e3ebf6f7cf74577
ksm <rkdtjd0909@gmail.com>
2024-03-03 21:53:01 +0900

    타임리프 기본 객체들
    - 스프링 부트 3.0이상 부터 ${#request} , ${#response} , ${#session} , ${#servletContext} 지원 안함 model에 담아서 사용

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -5,12 +5,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.Data;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 
 @Controller
 @RequestMapping("/basic")
@@ -46,6 +53,28 @@ public class BasicController {
     model.addAttribute("userMap", map);
 
     return "basic/variable";
+  }
+
+  @GetMapping("/basic-objects")
+  public String basicObject(
+    HttpSession session,
+    HttpServletRequest request,
+    HttpServletResponse response,
+    Model model
+  ) {
+    session.setAttribute("sessionData", "Hello Session");
+    model.addAttribute("request", request);
+    model.addAttribute("response", response);
+    model.addAttribute("servletContext", request.getServletContext());
+
+    return "basic/basic-objects";
+  }
+
+  @Component("helloBean")
+  static class HelloBean {
+    public String hello(String data) {
+      return "Hello " + data;
+    }
   }
 
   @Data
