186dd183e732e3f78dd1c8f5b78cd91c732ae500
ksm <rkdtjd0909@gmail.com>
2024-03-14 21:26:13 +0900

    타임리프 라디오 버튼

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -2,6 +2,7 @@ package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,6 +23,8 @@ public class FormItemController {
 
   private final ItemRepository itemRepository;
 
+  // @ModelAttribute 선언된 곳에서 따로 model에 담지 않아도,
+  // 자동으로 model에 담아서 view로 반환해줌
   @ModelAttribute("regions")
   public Map<String, String> regions() {
     Map<String, String> regions = new LinkedHashMap<>();
@@ -30,6 +33,11 @@ public class FormItemController {
     regions.put("JEJU", "제주");
 
     return regions;
+  }
+
+  @ModelAttribute("itemTypes")
+  public ItemType[] itemTypes() {
+    return ItemType.values();
   }
 
   @GetMapping
@@ -59,6 +67,7 @@ public class FormItemController {
 
     log.info("item.open={}", item.getOpen());
     log.info("item.regions={}", item.getRegions());
+    log.info("item.itemType={}", item.getItemType());
 
     Item savedItem = itemRepository.save(item);
     redirectAttributes.addAttribute("itemId", savedItem.getId());
