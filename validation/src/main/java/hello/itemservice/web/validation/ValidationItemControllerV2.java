3a0cd92064ef57d49d666f1f2a001647c65e8258
ksm <rkdtjd0909@gmail.com>
2024-03-24 19:51:35 +0900

    validator 분리2

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -12,6 +12,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -25,6 +27,11 @@ public class ValidationItemControllerV2 {
 
   private final ItemRepository itemRepository;
   private final ItemValidator itemValidator;
+
+  @InitBinder
+  public void init(WebDataBinder dataBinder) {
+    dataBinder.addValidators(itemValidator);
+  }
 
   @GetMapping
   public String items(Model model) {
@@ -233,7 +240,7 @@ public class ValidationItemControllerV2 {
     return "redirect:/validation/v2/items/{itemId}";
   }
 
-  @PostMapping("/add")
+  // @PostMapping("/add")
   public String addItemV5(
     @ModelAttribute Item item,
     BindingResult bindingResult,  // BindingResult bindingResult 파라미터의 위치는 @ModelAttribute Item item 다음에 와야 한다
@@ -242,6 +249,28 @@ public class ValidationItemControllerV2 {
   ) {
 
     itemValidator.validate(item, bindingResult);
+
+    // 검증 실패시 다시 입력 폼으로
+    if (bindingResult.hasErrors()) {
+      log.info("errors = {}", bindingResult);
+
+      return "validation/v2/addForm";
+    }
+
+    // 성공 로직
+    Item savedItem = itemRepository.save(item);
+    redirectAttributes.addAttribute("itemId", savedItem.getId());
+    redirectAttributes.addAttribute("status", true);
+    return "redirect:/validation/v2/items/{itemId}";
+  }
+
+  @PostMapping("/add")
+  public String addItemV6(
+    @Validated @ModelAttribute Item item,
+    BindingResult bindingResult,  // BindingResult bindingResult 파라미터의 위치는 @ModelAttribute Item item 다음에 와야 한다
+    RedirectAttributes redirectAttributes,
+    Model model
+  ) {
 
     // 검증 실패시 다시 입력 폼으로
     if (bindingResult.hasErrors()) {
