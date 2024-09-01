cda3fd1b5eff9deeb11b5e89f78ed9c097599422
ksm <rkdtjd0909@gmail.com>
2024-09-01 22:09:39 +0900

    등록, 조회 view화면 추가 및 기능 연결

--- src/main/java/spring/spring_mvc/controller/MemberController.java
+++ src/main/java/spring/spring_mvc/controller/MemberController.java
@@ -1,7 +1,13 @@
 package spring.spring_mvc.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import spring.spring_mvc.domain.Member;
 import spring.spring_mvc.service.MemberService;
 
 @Controller
@@ -11,5 +17,30 @@ public class MemberController {
 
   public MemberController(MemberService memberService) {
     this.memberService = memberService;
+  }
+
+  @GetMapping("/members/new")
+  public String createForm() {
+    return "members/createMemberForm";
+  }
+
+  @PostMapping("/members/new")
+  public String create(MemberFrom form) {
+    Member member = new Member();
+    member.setName(form.getName());
+
+    memberService.join(member);
+
+    return "redirect:/";
+  }
+
+  @GetMapping("/members")
+  public String list(
+    Model model
+  ){
+    List<Member> members = memberService.findMembers();
+
+    model.addAttribute("members", members);
+    return "members/memberList";
   }
 }
