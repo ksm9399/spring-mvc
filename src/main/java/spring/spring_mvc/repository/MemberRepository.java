6650e79b9ccd344f2a74125524c23c64933bcb85
ksm <rkdtjd0909@gmail.com>
2024-09-01 21:14:26 +0900

    회원 관리 예제
    - 간단하 회원 가입, 조회 기능 개발 및 테스트

---
+++ src/main/java/spring/spring_mvc/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package spring.spring_mvc.repository;
+
+import java.util.Optional;
+import java.util.List;
+
+import spring.spring_mvc.domain.Member;
+
+public interface MemberRepository {
+
+  Member save(Member member);
+  Optional<Member> findById(Long id);
+  Optional<Member> findByName(String name);
+  List<Member> findAll();
+}
