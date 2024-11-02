4bed3dd82db48f9d5d8d168873a18563d3352f53
ksm <rkdtjd0909@gmail.com>
2024-11-03 00:18:03 +0900

    새로운 할인 정책

---
+++ src/test/java/spring/spring_mvc/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,39 @@
+package spring.spring_mvc.discount;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import spring.spring_mvc.member.Grade;
+import spring.spring_mvc.member.Member;
+
+public class RateDiscountPolicyTest {
+
+  RateDiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
+
+  @Test
+  @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
+  void vip_o() {
+    // given
+    Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+    // when
+    int discount = rateDiscountPolicy.discount(member, 10000);
+
+    // then
+    Assertions.assertThat(discount).isEqualTo(1000);
+  }
+
+  @Test
+  @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
+  void vip_x() {
+    // given
+    Member member = new Member(1L, "memberBASIC", Grade.BASIC);
+
+    // when
+    int discount = rateDiscountPolicy.discount(member, 10000);
+
+    // then
+    Assertions.assertThat(discount).isEqualTo(0);
+  }
+}
