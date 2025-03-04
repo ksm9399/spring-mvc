b062b6966c3ec8b6b08c16e1fdc1895f8b6c2562
ksm <rkdtjd0909@gmail.com>
2025-03-04 18:40:59 +0900

    컴포넌스 스캔 필터터

---
+++ src/test/java/spring/spring_mvc/scan/filter/BeanA.java
@@ -0,0 +1,6 @@
+package spring.spring_mvc.scan.filter;
+
+@MyIncludeComponent
+public class BeanA {
+
+}
---
+++ src/test/java/spring/spring_mvc/scan/filter/BeanB.java
@@ -0,0 +1,6 @@
+package spring.spring_mvc.scan.filter;
+
+@MyExcludeComponent
+public class BeanB {
+
+}
